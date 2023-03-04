90ba066c3f44ca1dab1c484fa46ad3aefd51b598
C C Nipun Das <nipundas02@gmail.com>
2023-03-04 21:51:01 +0530

    .

---
+++ 2c-linearsearch.py
@@ -0,0 +1,18 @@
+list = []
+n = int(input("Enter no. of elements : "))
+for i in range(0, n):
+    print("Enter "+str(i+1)+"th element :", end="")
+    l = int(input())
+    list.append(l)
+print(list)
+
+
+f = 0
+key = int(input("Enter the key : "))
+for i in range(0,n):
+    if key== list[i]:
+        print("Found at location : ",(i+1))
+        f = 1
+        break
+if f==0:
+    print("Not found!")
