3f57f5f390979b32856431972ed9eb74a11ec6f5
C C Nipun Das <nipundas02@gmail.com>
2023-03-06 13:07:56 +0530

    .

--- 5b-findNthtermInFibo.py
+++ 5b-findNthtermInFibo.py
@@ -1,22 +1,22 @@
 # The Fibonacci numbers are the numbers in the following integer sequence.
 # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
 
-# no recursion
+
 def fibonacci(n):
 
     # Taking 1st two fibonacci numbers as 0 and 1
     f = [0, 1]
     print(f[0], "", f[1], " ", end="")
+
     for i in range(2, n + 1):
         f.append(f[i - 1] + f[i - 2])
         print(f[i - 1] + f[i - 2], " ", end="")
-        # print(str(f[i - 1] + f[i - 2]) + " ", end="")
-    print()    
+
+    print()
     return f[n]
 
 
-# using recursion
-# Python program to display the Fibonacci sequence
+###########################################################################
 
 
 def fibonacciRecursion(n):
@@ -24,6 +24,9 @@ def fibonacciRecursion(n):
         return n
     else:
         return fibonacciRecursion(n - 1) + fibonacciRecursion(n - 2)
+
+
+###########################################################################
 
 
 n = int(input("Enter limit to print upto : "))
