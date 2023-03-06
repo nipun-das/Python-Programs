f40e437b11aa4837a9701d6418cb3ace55e82e90
C C Nipun Das <nipundas02@gmail.com>
2023-03-06 14:12:51 +0530

    .

---
+++ 6c-findLargestinList.py
@@ -0,0 +1,14 @@
+list = []
+n = int(input("Enter no. of elements in list : "))
+for i in range(0, n):
+    print("Enter " + str(i + 1) + "th number : ", end="")
+    l = int(input())
+    list.append(l)
+print("List : ", list)
+
+max = list[0]
+for i in range(0, len(list)):
+    if max < list[i]:
+        max = list[i]
+
+print("Max in list is : ", max)
