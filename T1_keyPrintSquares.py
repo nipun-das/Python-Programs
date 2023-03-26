9dacc277c138a66be2480e950829c09f0bb53ef6
C C Nipun Das <nipundas02@gmail.com>
2023-03-26 14:31:07 +0530

    .

--- T1_keyPrintSquares.py
+++ T1_keyPrintSquares.py
@@ -1,11 +1,12 @@
 import math
 
-n = int(input("Enter the number of key-value pairs: "))
+# n = int(input("Enter the number of key-value pairs: "))
 
 dict = {}
 
-for i in range(n):
-    key = int(input("Enter the key: "))
+for i in range(1, 16):
+    # key = int(input("Enter the key: "))
+    key = i
     value = math.pow(key, 2)
     dict[key] = value
 
