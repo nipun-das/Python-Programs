f50d01c446773c4a53b4e277937b7c14d7901fdc
C C Nipun Das <nipundas02@gmail.com>
2023-07-18 23:04:13 +0530

    str pal

---
+++ Strings/strPalindromeWithoutSlicing.py
@@ -0,0 +1,14 @@
+def isPalindrome(str):
+    for i in range(0, int(len(str) / 2)):
+        if str[i] != str[len(str) - i - 1]:
+            return False
+    return True
+
+
+s = input("Enter a string")
+ans = isPalindrome(s)
+
+if ans:
+    print("Yes. String is palindrome")
+else:
+    print("No. String is not palindrome")
